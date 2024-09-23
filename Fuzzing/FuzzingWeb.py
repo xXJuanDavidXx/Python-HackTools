1c6a60947f0d3c45b7c18d68d3e562fb1f5ec32d
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-09-23 23:00:58 +0000

    Readme y Mejora del script de Fuzzing

--- Fuzzing/FuzzingWeb.py
+++ Fuzzing/FuzzingWeb.py
@@ -1,5 +1,5 @@
 import requests
-
+import time
 
 print ("""
 
@@ -22,17 +22,24 @@ print ("""
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
+
+[*]Fuzzing Web
        
-¡Descubre directorios ocultos en sitios web!
+       [+]Guia de uso
+            - Ingresa la dirección url del sitio web a probar
+            - Ingresa la dirección del diccionario en formato unix, por ejemplo: /usr/share/wordlists/SecLists/Discovery/Web-Content/directory-list-2.3-medium.txt
+       
 
        """)
 
+time.sleep(2)
 
-
-
+# Pide al usuario que ingrese la dirección url
 target_url = input("[+]Ingresa la dirección url: ")
+# Crea una lista para almacenar los directorios
 lista = []
 
+#Se Crea la función que va a hacer la petición al sitio web
 def response(url):
     try:
         return requests.get(url)
@@ -48,12 +55,12 @@ file_path = input("Ingrese la ubicación del archivo: ")
 # Usa la ubicación ingresada por el usuario para abrir el archivo
 try:
     with open(file_path, "r") as file:
-        for line in file.readlines():
-            word = line.strip()
+        for line in file.readlines():  # Lee cada línea del archivo (.readlines lo convierte en una lista)
+            word = line.strip() # Quita los espacios en blanco y los saltos de linea
             new_url = target_url + word  # Puede ser necesario quitar el "/" en caso de problemas
-            data = response(new_url)
+            data = response(new_url) #Le pasamos la url mas la nueva url para hacer la solicitud
 
-
+            # Verifica si la respuesta es 200 (OK)
             if data:
                 lista.append(new_url)
                 print("[+]Directorio encontrado. --> " + new_url)
@@ -62,7 +69,7 @@ try:
 except FileNotFoundError:
     print("El archivo no existe.")
 
-
+#Guardamos los directorios encontrados
 data_save = open("directorio_encontrado.txt","a")
 
 for directorio in lista:
